6a8b7529b499a5e55edd2f6b4701c74e40c61b0a
ss <1029515726@qq.com>
2024-01-20 15:07:10 +0800

    修改注释

--- src/main/java/com/qi/springbootinit/controller/WxMpController.java
+++ src/main/java/com/qi/springbootinit/controller/WxMpController.java
@@ -106,8 +106,7 @@ public class WxMpController {
         WxMenuButton wxMenuButton1SubButton1 = new WxMenuButton();
         wxMenuButton1SubButton1.setType(MenuButtonType.VIEW);
         wxMenuButton1SubButton1.setName("跳转页面");
-        wxMenuButton1SubButton1.setUrl(
-                "https://yupi.icu");
+        wxMenuButton1SubButton1.setUrl("");
         wxMenuButton1.setSubButtons(Collections.singletonList(wxMenuButton1SubButton1));
 
         // 菜单二
@@ -123,7 +122,7 @@ public class WxMpController {
         WxMenuButton wxMenuButton3SubButton1 = new WxMenuButton();
         wxMenuButton3SubButton1.setType(MenuButtonType.VIEW);
         wxMenuButton3SubButton1.setName("编程学习");
-        wxMenuButton3SubButton1.setUrl("https://yupi.icu");
+        wxMenuButton3SubButton1.setUrl("");
         wxMenuButton3.setSubButtons(Collections.singletonList(wxMenuButton3SubButton1));
 
         // 设置主菜单
